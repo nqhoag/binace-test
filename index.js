487bfe468c70e01c914419dc886cef8684535184
hoangnq <hoangnq@nal.vn>
2018-04-09 17:56:45 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -11,38 +11,84 @@ binance.options({
   // 'test':true
 });
 
+
 let currentTime = null;
 let buyHistory = [];
 
-setInterval(() => {
-	binance.prices((error, ticker) => {
-	  for (var key in ticker) {
-		
-		let coin_name = key
-		let main_coin = coin_name.slice(-3);
-		let main_price = 0;
-		if (main_coin == "BTC") {
-			main_price = 0.001;
-			// Buy 0.002 BTC
-		} else if (main_coin == "ETH") {
-			main_price = 0.0001;	
-		} else if (main_coin == "BNB") {
-			main_price = 1;
+binance.websockets.prevDay(false, (data) => {
+	let coin_name = data.symbol;
+	let open = data.open;
+	let main_coin = coin_name.slice(-3);
+	let main_price = 0;
+	if (main_coin == "BTC") {
+		main_price = 0.001;
+		// Buy 0.002 BTC
+	} else if (main_coin == "ETH") {
+		main_price = 0.02;	
+	} else if (main_coin == "BNB") {
+		main_price = 1;
+	}
+	if (main_price != 0) {
+		/*
+		*  Clear history and set new current Time
+		*/
+
+		// let currentTime = new Date()
+		// let day = currentTime.getDate()
+		// let month = currentTime.getMonth() + 1
+		// let year = currentTime.getFullYear()
+		// let currentDate = year + "/" + month + "/" + day;
+
+		let eventTime = new Date(data.closeTime);
+		let day = eventTime.getDate()
+		let month = eventTime.getMonth() + 1
+		let year = eventTime.getFullYear()
+		let eventDate = year + "/" + month + "/" + day;
+
+		if (currentTime !== eventDate) {
+			currentTime = eventDate;
+			buyHistory = [];
+			logFile.write('<br/>[INFO] ' + "Start for from " + new Date().toUTCString() + " to " + new Date(data.closeTime).toUTCString());
+			// console.log("Start for from ", new Date().toUTCString() , " to ", new Date(closeTime).toUTCString());
 		}
 
-		if (main_price != 0) {
-		  	check_coin(coin_name, main_price);
+		let currentType = 0;
+				  			
+		if (buyHistory[coin_name] !== undefined) {
+			currentType = buyHistory[coin_name];
 		}
-	  }
 
-	});
+		check_decrease(coin_name, data.percentChange, currentType);
 
-}, 10000);
+		if (data.percentChange > 99 && currentType < 99) {
+			let price = open - (99/100*open);
+			let quantity = parseInt(main_price/price) + 1;
+			buy(coin_name, quantity, price, 99);
+			return;
+		}
+
+		if (data.percentChange > 90 && currentType < 90) {
+			let price = open - (90/100*open);
+			let quantity = parseInt(main_price/price) + 1;
+			buy(coin_name, quantity, price, 90);
+			return;
+		}
+
+		if (data.percentChange > 50 && currentType < 50) {
+			let price = open - (50/100*open);
+			let quantity = parseInt(main_price/price) + 1;
+			buy(coin_name, quantity, price, 50);
+			return;
+		}
+
+  	}
+
+});
 
 
-function buy(coin_name, quantity, type) 
+function buy(coin_name, quantity , price, type) 
 {
-	binance.marketBuy(coin_name, quantity, (error, response) => {
+	binance.buy(coin_name, quantity, price, {type:'LIMIT'}, (error, response) => {
 		if (error) {
 			logFile.write('<br/>[ERROR] ' + error);
 		}
@@ -53,87 +99,20 @@ function buy(coin_name, quantity, type)
 	});
 }
 
-function get_price(coin_name, cb_func) 
-{
-	binance.prices(coin_name, (error, ticker) => {
-		cb_func(error, ticker)
-	});
-}
 
-function check_decrease(coin_name, change, open, currentType) 
+function check_decrease(coin_name, change, currentType) 
 {
 	for( let percent = 99; percent >= 9; percent = percent - 10) {
-		if ((change/open)*100 > percent && currentType < percent) {
+		if (change > percent && currentType < percent) {
 			// let price = open - (percent/100*open);
 			// let quantity = parseInt(main_price/price);
 			// buy(coin_name, quantity, percent);
 
 			logFile.write('<br/>[INFO] ' + coin_name + " has decrease > " + percent + "%");
-			// console.log("Notice: ", coin_name, " has decrease > ", percent, "%")
-			buyHistory[coin_name] = 40;
-
+			console.log("Notice: ", coin_name, " has decrease > ", change, "%")
+			buyHistory[coin_name] = percent;
 			return;
 		}
 	}
-}
-
-
-function check_coin(coin_name, main_price) 
-{
-	binance.candlesticks(coin_name, "1d", function(error, ticks) {
-		  		if (error) {
-		  			return;
-		  		}
-
-				if (ticks.length > 1) {
-					let last_tick = ticks[ticks.length - 1];
-					let [time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored] = last_tick;
-
-					/*
-			  		*  Clear history and set new current Time
-			  		*/
-			  		if (currentTime !== closeTime) {
-			  			currentTime = closeTime;
-			  			buyHistory = [];
-			  			logFile.write('<br/>[INFO] ' + "Start for from " + new Date().toUTCString() + " to " + new Date(closeTime).toUTCString());
-			  			// console.log("Start for from ", new Date().toUTCString() , " to ", new Date(closeTime).toUTCString());
-			  		}
-
-			  		let currentType = 0;
-			  		
-			  		if (buyHistory[coin_name] !== undefined) {
-			  			currentType = buyHistory[coin_name];
-			  		}
-
-					let change = open - close;
-					if (change > 0) {
-						
-						check_decrease(coin_name, change, open, currentType);
-
-						if ((change/open)*100 > 99.1 && currentType < 99.1) {
-							let price = open - (99.1/100*open);
-							let quantity = parseInt(main_price/price);
-							buy(coin_name, quantity, 99.1);
-							return;
-						}
-
-						if ((change/open)*100 > 90 && currentType < 90) {
-							let price = open - (90/100*open);
-							let quantity = parseInt(main_price/price);
-							buy(coin_name, quantity, 90);
-
-							return;
-						}
-
-						if ((change/open)*100 > 50 && currentType < 50) {
-							let price = open - (90/100*open);
-							let quantity = parseInt(main_price/price);
-							buy(coin_name, quantity, 50);
-
-							return;
-						}
-					}
-				}
-			}, {limit: 2});
 }
 
